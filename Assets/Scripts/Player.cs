0827d4285bdedddfeb354866133e48aef3ae6547
Anthony-Desktop <anthonymendez9@gmail.com>
2019-07-28 11:57:49 -0400

    I dunno what to make of this commit tbh

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Player : MonoBehaviour
+{
+
+    private Tilemap tilemap;
+
+    void Start()
+    {
+        tilemap = GetComponentInParent<Tilemap>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+    }
+
+    void HandleInput() {
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 clickPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int roundedPos = new Vector3Int((int)(clickPos.x), (int)(clickPos.y), 0);
+            TileBase tile = tilemap.GetTile(roundedPos);
+            
+            if (tile != null) {
+
+            }
+        }
+    }
+}
